8922eee9a23ec2a525bdb62cd6ddcd7e04284a39
venkat0830 <55931466+venkat0830@users.noreply.github.com>
2019-11-02 11:12:02 -0500

    First commit

---
+++ src/main/java/com/email/api/EmailNotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.email.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotifyApiApplication.class, args);
+	}
+
+}
